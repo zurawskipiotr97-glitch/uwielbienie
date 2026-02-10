dcacc315c1ada071d30497f23b3496a815a3452b
zurawskipiotr97-glitch <zurawskipiotr97@gmail.com>
2026-02-10 22:46:56 +0100

    Spring

---
+++ src/main/java/pl/zurawskipiotr97/proby/ProbyApiApplication.java
@@ -0,0 +1,13 @@
+package pl.zurawskipiotr97.proby;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProbyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProbyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/zurawskipiotr97/proby/ProbyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.zurawskipiotr97.proby;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProbyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
